56a714ba341776c8498411bd59da14ef3ad6adb2
calvang <calvin.huang5@gmail.com>
2020-07-01 18:07:08 -0400

    Update UI and file writing

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# wikipedia-table-web-scraper
-Simple web scraper using Beautiful Soup for collect data into .csv files.
+# Wikipedia Table Web Scraper
+Simple web scraper using Beautiful Soup for collect data into .csv and .json files.
+
+### Description:
+Created to scrape data from wikipedia for county centroids. I ended up updating some features so that it can run on many different Wikipedia tables.
+
+### Installation
+Set up your virtual environment and activate it:
+`python3 -m venv env`
+`source env/bin/activate`
+Install packages:
+`pip install -r requirements.txt`
+
+### Usage
+To run this script:
+```
+python3 wiki_scraper.py [OPTIONS]
+    -h,--help: open help menu
+    -v,--verbose: print verbose output
+    -c,--clean: remove all special characters from data
+    -o,--output <type>: output data format (csv, json), default output type is csv
+    -n,--name: name of the files to output to (minus file extension (will append an int if multiple
+    -url,--url: address of the wikipedia page with the table, default links to https://en.wikipedia.org/wiki/User:Michael_J/County_table
+```
--- wiki_scraper.py
+++ wiki_scraper.py
@@ -2,7 +2,6 @@ import csv
 import json
 import sys
 import os
-import contextlib
 import getopt
 import requests
 from bs4 import BeautifulSoup
@@ -10,34 +9,43 @@ from bs4 import BeautifulSoup
 # localized stdout silencer
 def disable_print(verbose):
     if not verbose:
-        f = open(os.devnull, "w") 
-        contextlib.redirect_stdout(f)
+        sys.stdout = open(os.devnull, 'w')
 
 def main():
     # read and process command line args
     try:
-        opts, args = getopt.getopt(sys.argv[1:],'hvco:',['help=','verbose=','clean=','output='])
+        opts, args = getopt.getopt(sys.argv[1:],'hvc:o:n:u:',['help','verbose','clean','output=','name=','url='])
     except getopt.GetoptError:
-        print('Usage: python3 wiki_scraper.py [OPTIONS] -o <type>')
+        print('Usage: python3 wiki_scraper.py [OPTIONS]')
         print('    Use -h for more options')
         sys.exit(2)
     verbose = False
     clean = False
+    unclean_chars = [';',':',',','#','!','&','*','+','-','–','°']
     output_type = 'csv'
+    output_file = 'output'
+    wikipedia_url = 'https://en.wikipedia.org/wiki/User:Michael_J/County_table'
     for opt, arg in opts:
         if opt in ('-h','--help'):
-            print('Usage: python3 wiki_scraper.py [OPTIONS] -o (--output)')
+            print('Usage: python3 wiki_scraper.py [OPTIONS]')
             print('    -h,--help: open help menu')
             print('    -v,--verbose: print verbose output')
             print('    -c,--clean: remove all special characters from data')
             print('    -o,--output <type>: output data format (csv, json), default output type is csv')
+            print('    -n,--name: name of the files to output to (minus file extension (will append an int if multiple')
+            print('    -url,--url: address of the wikipedia page with the table, default links to https://en.wikipedia.org/wiki/User:Michael_J/County_table')
             sys.exit(2)
-        elif opt in ('-v','--verbose'):
+        if opt in ('-v','--verbose'):
             verbose = True
-        elif opt in ('-c','--clean'):
-            clean = True        
-
-    wikipedia_url = 'https://en.wikipedia.org/wiki/User:Michael_J/County_table'
+        if opt in ('-c','--clean'):
+            clean = True
+        if opt in ('-o','--verbose'):
+            output_type = arg
+        if opt in ('-n','--name'):
+            output_file = arg       
+        if opt in ('-u','--url'):
+            wikipedia_url = arg     
+            print(wikipedia_url)   
 
     # get the html from the wikipedia page
     get_request = requests.get(wikipedia_url)
@@ -45,9 +53,10 @@ def main():
 
     disable_print(verbose)
 
+    name_int = 0
     # scrape each table dataset into a dictionary
     for table in soup.find_all('table'):
-        table_dict = {}
+        table_dicts = []
         # the following line was fun to write but probably a nightmare to read
         table_cols = [str(col.find(text=True, recursive=False)).strip() + \
                 ' ' + str(col.a.string).strip() \
@@ -61,16 +70,37 @@ def main():
         
         # insert row data into the dictionary
         col_num = 0
+        new_dict = {}
         for row in table.find_all('td'):
-            row_txt = str(row.find(text=True)).strip()
-            table_dict[table_cols[col_num]] = row_txt
+            row_txt = (''.join(i for i in str(row.find(text=True)).strip() if not i in unclean_chars)) \
+                 if clean else str(row.find(text=True)).strip()
+            new_dict[table_cols[col_num]] = row_txt
             print("Read '" + row_txt + "' from column " + str(col_num) + \
                     " with key '" + table_cols[col_num] + "'.")
             
             # wrap around columns
             col_num += 1
-            if col_num == len(table_cols[col_num]):
+            if col_num == len(table_cols):
+                table_dicts.append(new_dict)
+                new_dict = {}
                 col_num = 0
+
+        # write to output file
+        if name_int > 0:
+             output_file = output_file + str(name_int)
+        with open(output_file + '.' + output_type, mode='w') as outfile:
+            if output_type == 'csv':
+                writer = csv.DictWriter(outfile, table_cols)
+                writer.writeheader()
+                for i in table_dicts:
+                    writer.writerow(i)
+            elif output_type == 'json':
+                outfile.write(json.dumps(table_dicts,
+                        indent=4,
+                        sort_keys=True,
+                        ensure_ascii=False))
+            outfile.close()
+        name_int += 1
 
 if __name__ == '__main__':
     main()
